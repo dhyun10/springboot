b1203169be8b00ec25c9df45b72302150edbea67
DESKTOP-3SOV646\user <d-hyun10@hanmail.net>
2021-04-14 17:42:36 +0900

    Posts 등록 API

---
+++ src/main/java/com/dhyun/book/springboot/service/PostsService.java
@@ -0,0 +1,19 @@
+package com.dhyun.book.springboot.service;
+
+import com.dhyun.book.springboot.domain.posts.PostsRepository;
+import com.dhyun.book.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
